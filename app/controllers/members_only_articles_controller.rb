6f4abff6c96fbdd987fb98d2328d67ec6dff6222
Hpollard003 <hpollard003@gmail.com>
2021-10-14 16:34:14 -0400

    commit tree e77e17ba68772eda2ecb9d9090d0ad10a81224ce

--- app/controllers/members_only_articles_controller.rb
+++ app/controllers/members_only_articles_controller.rb
@@ -1,5 +1,8 @@
 class MembersOnlyArticlesController < ApplicationController
   rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
+  before_action :authorized
+
+
 
   def index
     articles = Article.where(is_member_only: true).includes(:user).order(created_at: :desc)
@@ -13,6 +16,10 @@ class MembersOnlyArticlesController < ApplicationController
 
   private
 
+  def authorized
+    return render json: { error: "Not authorized" }, status: :unauthorized unless session.include? :user_id
+  end
+  
   def record_not_found
     render json: { error: "Article not found" }, status: :not_found
   end
